cd00e1b55e4c0b390e3ca039943a6817a4a5d6c7
Guruansh Singh <gs474@drexel.edu>
2019-03-08 16:50:20 -0500

    Fixed bugs with adding arguments

--- redis_interface.py
+++ redis_interface.py
@@ -1,7 +1,7 @@
 import redis
 import dill
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0) # Make connection to Redis instance running locally
+redis_client = redis.Redis(host='35.193.40.68', port=6379, db=0) # Make connection to Redis instance running locally
 
 def add_args_to_gds(task_id,list_of_args):
 
@@ -9,7 +9,7 @@ def add_args_to_gds(task_id,list_of_args):
 	if check_key_exists_in_gds(task_id): # Returns number of keys found - should be 0
 		raise Exception("Task ID already has arguments/results stored in the GDS") # Temporary - Replace by EtherFx error
 	
-	return insert_in_key_list_redis(list_of_args)
+	return insert_in_key_list_redis(task_id,list_of_args)
 
 def get_args_from_gds(task_id):
 
@@ -20,24 +20,25 @@ def get_args_from_gds(task_id):
 def retrieve_value_list_for_key_gds(redis_key):
 
 	number_of_vals = redis_client.llen(redis_key)
-	vals_retrieved = redis_client.lrange(redis_key, 0,number_of_args-1)
+	vals_retrieved = redis_client.lrange(redis_key, 0,number_of_vals-1)
 
-	return args_retrieved if args_retrieved else None
+	return vals_retrieved if vals_retrieved else None
 
 
 def check_key_exists_in_gds(task_id):
-	if redis_client.exist(task_id) > 0:
+	if redis_client.exists(task_id) > 0:
 		return True
 	else:
 		return False
 
-def insert_in_key_list_redis(values_to_be_added):
+def insert_in_key_list_redis(task_id,values_to_be_added):
 	
 	try:
 		for x in values_to_be_added:
-		redis_client.rpush(task_id,byte_arg)
-	except:
-		return False
+			#print type(x)
+			redis_client.rpush(task_id,x)
+	except Exception as e:
+		print e
 	return True
 
 
@@ -71,10 +72,11 @@ def clear_gds_for_task_id(task_id):
 
 
 def main():
-	test_obj = sample_class()
+	#test_obj = sample_class()
 	args = ["abc", "def", "ghi"]
-	clear_gds_for_task_id("fooda")
-	add_args_to_gds("fooda", args)
+	#clear_gds_for_task_id("fooda")
+	#print add_args_to_gds("fooda", args)
+	print retrieve_value_list_for_key_gds("fooda")
 	#print get_args_from_gds("fooda")
 if __name__ == '__main__':
 	main()
